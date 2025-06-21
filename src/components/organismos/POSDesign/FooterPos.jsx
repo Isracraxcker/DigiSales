f9b282843882adaa02ee7248915109e40df37b3d
Isracraxcker <andyisraeljaramillo@gmail.com>
2025-06-21 12:40:09 -0500

    fix: Añadi funcion eliminar y iconTrash

--- src/components/organismos/POSDesign/FooterPos.jsx
+++ src/components/organismos/POSDesign/FooterPos.jsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 import { Device } from "../../../styles/breakpoints";
 import { Btn1 } from "../../moleculas/Btn1";
+import { Trash2 } from "lucide-react";
+import { useCartVentasStore } from "../../../store/CartVentasStore";
 export function FooterPos() {
+
+ const {resetState} = useCartVentasStore()
   return (
     <Footer>
       <article className="content">
-        <Btn1 titulo="Eliminar" />
+        <Btn1 bgcolor={"#ff0000"} color={"255,255,255"} funcion={resetState} titulo="Eliminar venta" icono={<Trash2 size={20} />} />
         <Btn1 titulo="Ver ventas del día y Devoluciones" />
       </article>
     </Footer>
