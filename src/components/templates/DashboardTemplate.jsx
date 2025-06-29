e976dd76bdbcddc38a339f1f805a8a6ebad337ce
Isracraxcker <andyisraeljaramillo@gmail.com>
2025-06-29 12:43:07 -0500

    fix: Dashboard - Card Totales

--- src/components/templates/DashboardTemplate.jsx
+++ src/components/templates/DashboardTemplate.jsx
@@ -1,12 +1,40 @@
 import styled from "styled-components";
 import { Device } from "../../styles/breakpoints";
 import { DashboardHeader } from "../organismos/DashboardDesign/DashboardHeader";
+import { CardTotales } from "../organismos/DashboardDesign/CardTotales";
 export const DashboardTemplate = () => {
   return (
     <Container>
       <DashboardHeader />
       <MainContent>
-        <Area1>1</Area1>
+        <Area1>
+          <ContentTotales>
+            <CardTotales
+              title="Ventas"
+              icon="iconamoon:wallet-1-fill"
+              value="1,000,000"
+              percentage={10}
+            />
+          </ContentTotales>
+
+          <ContentTotales>
+            <CardTotales
+              title="Ingresos"
+              icon="iconamoon:wallet-1-fill"
+              value="1,000,000"
+              percentage={-10}
+            />
+          </ContentTotales>
+
+          <ContentTotales>
+            <CardTotales
+              title="Costos"
+              icon="iconamoon:wallet-1-fill"
+              value="1,000,000"
+              percentage={0}
+            />
+          </ContentTotales>
+        </Area1>
         <Area2>2</Area2>
         <Area3>3</Area3>
         <Area4>4</Area4>
@@ -44,7 +72,6 @@ const MainContent = styled.div`
 `;
 const Area1 = styled.section`
   grid-area: area1;
-  /* background-color: rgba(242, 8, 242, 0.2); */
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 20px;
@@ -54,7 +81,6 @@ const Area1 = styled.section`
 `;
 const Area2 = styled.section`
   grid-area: area2;
-  /* background-color: rgba(17, 228, 84, 0.2); */
   border: 2px solid ${({ theme }) => theme.bordercolorDash};
   box-shadow: ${({ theme }) => theme.boxshadow};
   border-radius: 20px;
@@ -65,11 +91,9 @@ const Area3 = styled.section`
   background-color: ${({ theme }) => theme.body};
   border: 2px solid ${({ theme }) => theme.bordercolorDash};
   border-radius: 20px;
-  /* background-color: rgba(21, 56, 231, 0.2); */
 `;
 const Area4 = styled.section`
   grid-area: area4;
-
   display: flex;
   gap: 20px;
   flex-wrap: wrap;
