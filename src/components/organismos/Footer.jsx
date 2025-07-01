be17f26ba9e00b246c5a0e387a4d3d72c25e9364
Isracraxcker <andyisraeljaramillo@gmail.com>
2025-06-30 21:05:55 -0500

    complete: Footer Login - Terminado

--- src/components/organismos/Footer.jsx
+++ src/components/organismos/Footer.jsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 import { GiPadlock } from "react-icons/gi";
+import { ShieldCheck } from "lucide-react";
 export function Footer() {
   return (
     <Container>
       <section className="lock">
-        <GiPadlock />
+        <ShieldCheck size={30}  />
         <span>
-          Esta es una página segura de andy-jaramillo.vercel.app Si tienes dudas sobre la
+          Esta es una página segura de AICODIGI Si tienes dudas sobre la
           autenticidad de la web, comunícate con
-          <br /> nosotros al 099-084-6008 o a través de nuestros medios digitales.
+          <br /> nosotros a través de nuestros medios digitales.
         </span>
       </section>
       <section className="derechos">
@@ -16,7 +17,7 @@ export function Footer() {
         <div className="separador"></div>
         <span>Todos los derechos reservados</span>
         <div className="separador"></div>
-        <span>© 2025 andy-jaramillo.vercel.app</span>
+        <span>© 2025 AICODIGI - Andy Jaramillo</span>
       </section>
     </Container>
   );
@@ -26,27 +27,27 @@ const Container = styled.div`
   flex-direction: column;
   font-size: 12.2px;
   color: #91a4b7;
-  gap:5px;
-  margin:10px;
+  gap: 5px;
+  margin: 10px;
   .lock {
-    border-bottom: 1px solid rgba(145, 164, 183,0.3);
-    gap:5px;
-    display:flex;
-    align-items:center;
+    border-bottom: 1px solid rgba(145, 164, 183, 0.3);
+    gap: 5px;
+    display: flex;
+    align-items: center;
   }
   .derechos {
     display: flex;
     justify-content: space-between;
-   .separador{
-    width:1px;
-    background-color:rgba(145, 164, 183,0.3);
-    margin-top:4px;
-    height:80%;
-    align-items:center;
-    display:flex;
-   }
-    span{
-      margin-top:5px;
+    .separador {
+      width: 1px;
+      background-color: rgba(145, 164, 183, 0.3);
+      margin-top: 4px;
+      height: 80%;
+      align-items: center;
+      display: flex;
+    }
+    span {
+      margin-top: 5px;
     }
   }
 `;
