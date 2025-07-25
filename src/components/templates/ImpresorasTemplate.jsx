1f47760cc003d0b1199bb61ad602dea152982851
Isracraxcker <andyisraeljaramillo@gmail.com>
2025-07-25 13:40:58 -0500

    fix: impresoras aun no termina

--- src/components/templates/ImpresorasTemplate.jsx
+++ src/components/templates/ImpresorasTemplate.jsx
@@ -1,8 +1,118 @@
 import styled from "styled-components";
-import { slideBackground } from "../../styles/keyframes";
 import { Btn1 } from "../moleculas/Btn1";
+import { slideBackground } from "../../styles/keyframes";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useImpresorasStore } from "../../store/ImpresorasStore";
+import { BarLoader } from "react-spinners";
+import { Switch } from "../ui/toggles/Switch";
+import { SelectList } from "../ui/lists/SelectList";
+import { useAsignacionCajaSucursalStore } from "../../store/AsignacionCajaSucursalStore";
+import { useState } from "react";
+import ticket from "../../reports/TicketPrueba";
+import {HeaderImpresoras} from "../organismos/ImpresorasDesign/HeaderImpresoras"
+import toast from "react-hot-toast";
 export const ImpresorasTemplate = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const {
+    mostrarDatosPc,
+    statePrintDirecto,
+    setStatePrintDirecto,
+    mostrarListaImpresoraLocales,
+    selectImpresora,
+    setSelectImpresora,
+    editarImpresoras,
+    mostrarImpresoraXCaja,
+  } = useImpresorasStore();
+  const { dataSucursalesAsignadas } = useAsignacionCajaSucursalStore();
+  const queryClient = useQueryClient();
+  //mostrar impresoras por caja
+  const {
+    data: dataImpresorasPorCaja,
+    isLoading: isLoadingImpresoras,
+    error: errorImpresoras,
+  } = useQuery({
+    queryKey: [
+      "mostrar impresora por caja",
+      {
+        id_caja: dataSucursalesAsignadas?.id_caja,
+      },
+    ],
+    queryFn: () =>
+      mostrarImpresoraXCaja({
+        id_caja: dataSucursalesAsignadas?.id_caja,
+      }),
+  });
+  //mostrar datos de Pc local
+  const {
+    data: dataPcLocal,
+    isLoading: isLocadingDatosPc,
+    error: errorDatosPc,
+  } = useQuery({
+    queryKey: ["mostrar datos de PC"],
+    queryFn: mostrarDatosPc,
+  });
+  //mostrar las impresoras locales
+  const {
+    data: dataImpresorasLocales,
+    isLoading: isloadingImpresorasLocales,
+    error: errorImpresorasLocales,
+  } = useQuery({
+    queryKey: ["mostrar lista impresoras locales"],
+    queryFn: mostrarListaImpresoraLocales,
+    enabled: !!dataPcLocal,
+  });
+  //editar impresoras
+  const { mutate: doEditar, isPending } = useMutation({
+    mutationKey: ["editar impresoras"],
+    mutationFn: editar,
+    onError: (error) => {
+      toast.error("Error al editar impresoras" + error.message);
+    },
+    onSuccess: () => {
+      toast.success("Datos guardados");
+      queryClient.invalidateQueries(["mostrar impresora por caja"]);
+    },
+  });
+  async function editar() {
+    const p = {
+      id: dataImpresorasPorCaja?.id,
+      state: statePrintDirecto,
+      pc_name: dataPcLocal?.machineName,
+      ip_local: dataPcLocal?.localIPs[0],
+      name: selectImpresora?.name,
+    };
+    await editarImpresoras(p);
+  }
+  const probarTicket = async () => {
+    const response = await ticket("b64");
+    // Convertir el contenido base64 en un archivo Blob
+    const binaryString = atob(response.content);
+    const binaryLen = binaryString.length;
+    const bytes = new Uint8Array(binaryLen);
+    for (let i = 0; i < binaryLen; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    const blob = new Blob([bytes], { type: "application/pdf" });
+    // Crear un archivo simulando un archivo subido
+    const file = new File([blob], "GeneratedTicket.pdf", {
+      type: "application/pdf",
+    });
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("printerName", selectImpresora?.name);
+    const responseApi = await fetch("http://localhost:5075/api/print-ticket", {
+      method: "POST",
+      body: formData,
+    });
+    if (responseApi.ok) {
+      toast.success("El PDF se envió a imprimir correctamente.");
+    } else {
+      const error = await responseApi.text();
+      toast.error("Error al imprimir" + error);
+    }
+  };
   const descargarArchivo = (ruta) => {
     const link = document.createElement("a");
     link.href = ruta;
@@ -12,35 +122,82 @@ export const ImpresorasTemplate = () => {
     link.click();
     document.body.removeChild(link);
   };
-
+  const isLoading = isLocadingDatosPc;
+  const error = errorDatosPc;
+  if (isLoading) {
+    return <BarLoader color="#b7b7b7" />;
+  }
   return (
     <Container>
-      <SubContainer>
-        <Title>IMPRESORAS</Title>
-        <SubTitle>Imprimir directo</SubTitle>
-        <Avatar $bg="#d70e79">
-          <Btn1
-            funcion={() =>
-              descargarArchivo(
-                "https://drive.google.com/file/d/1GgLCWahmtrrXjXoN-5TYlVEY-X9Ooxy1/view?usp=sharing"
-              )
-            }
-            titulo={"Descargar"}
-          />
-          <span className="nombre">Ver Tutorial</span>
-        </Avatar>
-        <span className="descripcion">
-          Servicio para imprimir directo a impresoras.
-        </span>
-        <section className="advertencia">
-          <Icon className="icono" icon="svg-spinners:clock" />
-          <span>Si ya instalo, actualice esta pagina.</span>
-        </section>
-      </SubContainer>
+      
+      {dataPcLocal ? (
+        <SubContainer>
+          <HeaderImpresoras/>
+          <Title>IMPRESORAS</Title>
+          <ContentSwich>
+            <SubTitle>Imprimir directo</SubTitle>
+            <Switch
+              setState={() => {
+                setStatePrintDirecto();
+                doEditar();
+              }}
+              state={statePrintDirecto}
+            />
+          </ContentSwich>
+
+          <Avatar $bg="#d70e79">
+            {statePrintDirecto ? (
+              <>
+                <Btn1 funcion={probarTicket} bgcolor={"#d70e79"} color={"#fff"} titulo={"probar"} />
+                <SelectList
+                  itemSelect={selectImpresora}
+                  onSelect={setSelectImpresora}
+                  data={dataImpresorasLocales}
+                  displayField="name"
+                />
+                <Btn1
+                  funcion={doEditar}
+                  disabled={isPending}
+                  bgcolor={"#fff"}
+                  titulo={"Guardar"}
+                />
+              </>
+            ) : (
+              <span className="anuncio">
+                {" "}
+                se mostrara un cuadro de diálogo al momento de imprimir
+              </span>
+            )}
+          </Avatar>
+        </SubContainer>
+      ) : (
+        <SubContainer>
+          <Title>IMPRESORAS</Title>
+          <SubTitle>Descargue e instale el servidor</SubTitle>
+          <Avatar $bg="#1424a0">
+            <Btn1
+              funcion={() =>
+                descargarArchivo(
+                  "https://drive.google.com/file/d/1v5Q632tEWx8yTlfnC4HYT6SsGKnORduw/view?usp=sharing"
+                )
+              }
+              titulo={"descargar"}
+            />
+            <span className="nombre">ver tutorial</span>
+          </Avatar>
+          <span className="descripcion">
+            servicio para imprimir directo a impresoras térmicas
+          </span>
+          <br></br>
+          <section className="advertencia">
+            <Icon className="icono" icon="svg-spinners:clock" />
+            <span>si ya instalo, actualice esta pagina.</span>
+          </section>
+        </SubContainer>
+      )}
     </Container>
   );
 };
-
 const ContentSwich = styled.section`
   display: flex;
   gap: 15px;
---
+++ src/components/ui/toggles/Switch.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function Switch({ state, setState }) {
+  return (
+    <StyledWrapper>
+      <input
+        type="checkbox"
+        className="checkbox"
+        id="checkbox"
+        checked={state}
+        onClick={setState}
+      />
+      <label className="switch" htmlFor="checkbox">
+        <span className="slider" />
+      </label>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  width: 51px;
+  height: 31px;
+  position: relative;
+
+  .checkbox {
+    opacity: 0;
+    width: 0;
+    height: 0;
+    position: absolute;
+  }
+
+  .switch {
+    width: 100%;
+    height: 100%;
+    display: block;
+    background-color: #e9e9eb;
+    border-radius: 16px;
+    cursor: pointer;
+    transition: all 0.2s ease-out;
+  }
+
+  .slider {
+    width: 27px;
+    height: 27px;
+    position: absolute;
+    left: 2px;
+    top: 2px;
+    border-radius: 50%;
+    background: #ffffff;
+    box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.15),
+      0px 3px 1px rgba(0, 0, 0, 0.06);
+    transition: all 0.2s ease-out;
+    cursor: pointer;
+  }
+
+  .checkbox:checked + .switch {
+    background-color: #34c759;
+  }
+
+  .checkbox:checked + .switch .slider {
+    transform: translateX(20px);
+  }
+`;
